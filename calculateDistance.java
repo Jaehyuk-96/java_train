54667f0c5688241a446905bb7df5425e3c73f924
jaehyuck000 <jaehyuck009603@gmail.com>
2023-10-12 22:51:45 +0900

    두점 사이의 거리를 계산하는 메서드

---
+++ calculateDistance.java
@@ -0,0 +1,9 @@
+public class calculateDistance {
+    static double getDistance(int x, int y, int x1, int y1) {
+        return Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));//x,y는 지역변수
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDistance(1, 1, 2, 2));
+    }
+}
