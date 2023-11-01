c0de926eb8ed35fa999e53aa4155700e0f2bf76b
jaehyuck000 <jaehyuck009603@gmail.com>
2023-11-01 23:12:40 +0900

    향상된 for문 사용해서 배열 나열하기

---
+++ java_practice200/chapter3/ForeachLotto.java
@@ -0,0 +1,9 @@
+public class ForeachLotto {
+    public static void main(String[] args) {
+        int[]mm = {1,6,16,22,23,33}; //배열 생성
+        for(int m :  mm){//mm의 배열을 m의 값으로 꺼내옴
+            System.out.println(m+"\t");//m을 하나씩 나열
+        }
+        System.out.println();
+    }
+}
